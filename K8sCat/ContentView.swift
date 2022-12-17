386194e6fd205d229ddd15d629564004fb3d1e1f
晒晕的兔子 <gubu1986@126.com>
2022-12-17 21:45:05 +0800

    Initial Commit

---
+++ K8sCat/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  K8sCat
+//
+//  Created by 顾艳华 on 2022/12/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ K8sCat/K8sCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  K8sCatApp.swift
+//  K8sCat
+//
+//  Created by 顾艳华 on 2022/12/17.
+//
+
+import SwiftUI
+
+@main
+struct K8sCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
