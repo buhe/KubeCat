8d8e58467403cccf931aa1ffc3c2d624d4178395
晒晕的兔子 <gubu1986@126.com>
2022-12-20 16:00:52 +0800

    tabbar

--- K8sCat/ContentView.swift
+++ K8sCat/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            Text("ns").tabItem {
+                Text("ns")
+            }
+            
+            Text("global").tabItem {
+                Text("global")
+            }
+            
+            Text("settings").tabItem {
+                Text("settings")
+            }
         }
-        .padding()
     }
 }
 
