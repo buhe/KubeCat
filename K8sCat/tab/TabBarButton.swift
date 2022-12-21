41baa262ef4378f9cad1afabd7d1ba17d08406f7
buhe <bugu1986@126.com>
2022-12-21 21:06:31 +0800

    git-u:update

--- K8sCat/tab/TabBarButton.swift
+++ K8sCat/tab/TabBarButton.swift
@@ -22,14 +22,38 @@ struct TabBarButton: View {
 struct CustomTopTabBar: View {
     @Binding var tabIndex: Int
     var body: some View {
-        HStack(spacing: 20) {
-            TabBarButton(text: "Pods", isSelected: .constant(tabIndex == 0))
-                .onTapGesture { onButtonTapped(index: 0) }
-            TabBarButton(text: "Job", isSelected: .constant(tabIndex == 1))
-                .onTapGesture { onButtonTapped(index: 1) }
-            TabBarButton(text: "Deployments", isSelected: .constant(tabIndex == 2))
-                .onTapGesture { onButtonTapped(index: 2) }
-            Spacer()
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 20) {
+                Group {
+                    TabBarButton(text: "Pods", isSelected: .constant(tabIndex == 0))
+                        .onTapGesture { onButtonTapped(index: 0) }
+                    TabBarButton(text: "Deployments", isSelected: .constant(tabIndex == 1))
+                        .onTapGesture { onButtonTapped(index: 1) }
+                    TabBarButton(text: "Job", isSelected: .constant(tabIndex == 2))
+                        .onTapGesture { onButtonTapped(index: 2) }
+                    TabBarButton(text: "Cron Job", isSelected: .constant(tabIndex == 3))
+                        .onTapGesture { onButtonTapped(index: 3) }
+                    TabBarButton(text: "Statefull Sets", isSelected: .constant(tabIndex == 4))
+                        .onTapGesture { onButtonTapped(index: 4) }
+                    TabBarButton(text: "Service", isSelected: .constant(tabIndex == 5))
+                        .onTapGesture { onButtonTapped(index: 5) }
+                }
+                Group {
+                    TabBarButton(text: "Config Map", isSelected: .constant(tabIndex == 6))
+                        .onTapGesture { onButtonTapped(index: 6) }
+                    TabBarButton(text: "Secrets", isSelected: .constant(tabIndex == 7))
+                        .onTapGesture { onButtonTapped(index: 7) }
+                    TabBarButton(text: "Daemon Sets", isSelected: .constant(tabIndex == 8))
+                        .onTapGesture { onButtonTapped(index: 8) }
+                    TabBarButton(text: "Daemon Sets", isSelected: .constant(tabIndex == 9))
+                        .onTapGesture { onButtonTapped(index: 9) }
+                    TabBarButton(text: "Daemon Sets", isSelected: .constant(tabIndex == 10))
+                        .onTapGesture { onButtonTapped(index: 10) }
+                    TabBarButton(text: "Daemon Sets", isSelected: .constant(tabIndex == 11))
+                        .onTapGesture { onButtonTapped(index: 11) }
+                }
+              
+            }
         }
         .border(width: 1, edges: [.bottom], color: .black)
     }
