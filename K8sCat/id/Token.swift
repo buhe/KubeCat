9c8cb8b1f7d0c951523a92fac8ef677d53b46402
buhe <bugu1986@126.com>
2023-01-07 21:22:52 +0800

    git-u:update

--- K8sCat/id/Token.swift
+++ K8sCat/id/Token.swift
@@ -26,8 +26,41 @@ public struct TokenRequest: AWSEncodableShape {
 }
 
 extension STS {
-    public func getCallerIdentityToken(_ input: TokenRequest, logger: Logger = AWSClient.loggingDisabled, on eventLoop: EventLoop? = nil) -> EventLoopFuture<GetCallerIdentityResponse> {
+    public func getCallerIdentityToken(_ input: TokenRequest, logger: Logger = AWSClient.loggingDisabled, on eventLoop: EventLoop? = nil) -> EventLoopFuture<TokenResponse> {
         return self.client.execute(operation: "GetCallerIdentity", path: "/", httpMethod: .GET, serviceConfig: self.config, input: input, logger: logger, on: eventLoop)
+    }
+}
+
+public struct TokenResponse: AWSDecodableShape {
+    let kind: String?
+    let apiVersion: String?
+    let status: Status?
+    
+    private enum CodingKeys: String, CodingKey {
+        case kind = "kind"
+        case apiVersion = "apiVersion"
+        case status
+    }
+    
+    public init(king: String? = nil, apiVersion: String? = nil, status: Status? = nil) {
+        self.kind = king
+        self.apiVersion = apiVersion
+        self.status = status
+    }
+}
+
+public struct Status: AWSDecodableShape {
+    let expirationTimestamp: String?
+    let token: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case expirationTimestamp = "expirationTimestamp"
+        case token = "token"
+    }
+    
+    public init(expirationTimestamp: String? = nil, token: String? = nil) {
+        self.expirationTimestamp = expirationTimestamp
+        self.token = token
     }
 }
 
