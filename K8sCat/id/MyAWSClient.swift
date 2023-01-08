c6029021abbe308f6a3fe2551d5c831c72087a5c
buhe <bugu1986@126.com>
2023-01-08 17:06:00 +0800

    git-u:update

--- K8sCat/id/MyAWSClient.swift
+++ K8sCat/id/MyAWSClient.swift
@@ -64,9 +64,33 @@ struct MyAWSClient {
             credentialProvider: .static(accessKeyId: ak, secretAccessKey: sk),
             httpClientProvider: .createNew
         )
-        let sts = STS(client: client, region: .uswest1)
-        let url = try! sts.signURL(
-            url: URL(string: sts.endpoint+"/?Action=GetCallerIdentity&Version=2011-06-15")!,
+        var sts: STS?
+        switch region {
+        case "us-west-1": sts = STS(client: client, region: .uswest1)
+        case "us-east-1": sts = STS(client: client, region: .useast1)
+        case "us-east-2": sts = STS(client: client, region: .useast2)
+        case "us-west-2": sts = STS(client: client, region: .uswest2)
+        case "ca-central-1": sts = STS(client: client, region: .cacentral1)
+        case "eu-west-1": sts = STS(client: client, region: .euwest1)
+        case "eu-west-2": sts = STS(client: client, region: .euwest2)
+        case "eu-west-3": sts = STS(client: client, region: .euwest3)
+        case "eu-south-1": sts = STS(client: client, region: .eusouth1)
+        case "eu-south-2": sts = STS(client: client, region: .eusouth2)
+        case "eu-central-1": sts = STS(client: client, region: .eucentral1)
+        case "eu-central-2": sts = STS(client: client, region: .eucentral2)
+        case "ap-northeast-1": sts = STS(client: client, region: .apnortheast1)
+        case "ap-northeast-2": sts = STS(client: client, region: .apnortheast2)
+        case "ap-northeast-3": sts = STS(client: client, region: .apnortheast3)
+        case "ap-southeast-1": sts = STS(client: client, region: .apsoutheast1)
+        case "ap-southeast-2": sts = STS(client: client, region: .apsoutheast2)
+        case "ap-southeast-3": sts = STS(client: client, region: .apsoutheast3)
+        case "ap-south-1": sts = STS(client: client, region: .apsouth1)
+        case "ap-south-2": sts = STS(client: client, region: .apsouth2)
+        case "sa-east-1": sts = STS(client: client, region: .saeast1)
+        default: break
+        }
+        let url = try! sts!.signURL(
+            url: URL(string: sts!.endpoint+"/?Action=GetCallerIdentity&Version=2011-06-15")!,
             httpMethod: .GET,
             headers: ["x-k8s-aws-id": clusterName],
             expires: .seconds(60)
