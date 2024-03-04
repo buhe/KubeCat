de0ce250301040041008fe92663b0a47e160a0b1
buhe <bugu1986@126.com>
2024-03-04 11:52:52 +0800

    "Refactor ClusterView.swift to handle saving and selecting namespaces in viewContext."

--- K8sCat/cluster/ClusterView.swift
+++ K8sCat/cluster/ClusterView.swift
@@ -120,6 +120,8 @@ struct ClusterView: View {
             do {
                 try viewContext.save()
                 viewModel.select(viewContext: viewContext)
+                let namespaces = viewModel.namespace
+                viewModel.ns = namespaces.isEmpty ? "default" : namespaces.first!
             } catch {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
